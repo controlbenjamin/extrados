6a7458b799eface2353dac0241532a6bfb323acd
Your Name <you@example.com>
2020-04-18 13:53:34 -0300

    primer commit

---
+++ Extrados/Controllers/ProductoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Extrados.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Tabla()
+        {
+            return View();
+        }
+
+        public JsonResult ListarUsuarios()
+        {
+
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
